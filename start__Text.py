c44ac97f9ad447e40b8af7e750d9e165776c8346
teamdabang <dgjg45768@gmail.com>
2025-01-31 07:24:51 +0530

    Create start__Text.py

---
+++ start__Text.py
@@ -0,0 +1,116 @@
+Hello {message.from_user.mention} I am one and only DRM Downloader Bot on Telegram.
+
+You can use me to Download DRM protected links to Telegram
+
+Here I support Direct DRM links of Zee5, Hotstar etc..................
+
+I can also DRM protected links transloaded from @@X1Xeditz
+
+If you found any issue please contact Support @X1XEDITZ**
+
+, reply_markup=InlineKeyboardMarkup(ST1)
+
+[ 
+    [
+        InlineKeyboardButton(text="Updates Channel", url="https://t.me/jv"),
+        InlineKeyboardButton(text="Support Grp", url="https://t.me/jv")
+    ],
+    [
+        InlineKeyboardButton(f"About", callback_data="About"),
+        InlineKeyboardButton(f"Help", callback_data="Help"),
+        InlineKeyboardButton(f"Contact Us", callback_data="ContactUs"),
+        InlineKeyboardButton(f"OTTS", callback_data="OTTS"),
+    ],
+    [
+        InlineKeyboardButton(f"Usage", callback_data="usage"),
+        InlineKeyboardButton(f"Plans", callback_data="plans"),   
+    ]
+]
+
+About_TEXT = "🄳🅁🄼 🄳🄾🅆🄽🄻🄾🄳🄴 🄱🄾🅃
+
+  ➺ My Name  : DRM Downloader Bot
+  ➺ Version      :  `v1.0.0`
+  ➺ Language  : `English`
+  ➺ Owner        : `UV`
+  ➺ Release     : `India`
+  ➺ Developer  : @X1Xeditz
+
+╚════════✧❁✧════════➩"
+
+Help_TEXT = "Here You can find all available Commands:-
+    /start :- To start The Bot.
+    /help :- Show Help & Features.
+    /features :- See available features.
+    /plans :- See available plans.
+    /myplan :- To check your plan details.
+    /otts :- To check available OTTs.
+    /settings :- To Edit Settings.
+
+1. Send url (example.domain/File.mp4 | New Filename.mp4).
+2. Send Image As Custom Thumbnail (Optional).
+3. Select the button (Quality).
+4.Settings Command Available For Editing Caption And Upload Settings.
+5.Custom Drive Upload Method Available.
+
+Just send me any DRM links from supported sites to download That I can also Upload To Google Drive..."
+
+ContactUs_TEXT = "Contact: @x1xEdiz"
+
+OTTS_TEXT = "Available OTTs
+
+1. ALTT
+2. ARY Digital
+3. Addatimes
+4. Aha
+5. Amazon miniTV
+6. Atrangii
+7. Bongo BD
+8. Chaupal
+9. Chorki
+10. Dangal Play
+11. Discovery+
+12. EPIC ON
+13. ETV Win
+14. Eros Now
+15. HBO Max
+16. Hoichoi
+17. Hotstar
+18. Hulu
+19. Hungama
+20. JioCinema
+21. KLiKK
+22. Kuku FM
+25. Lionsgate Play
+24. MUBI
+26. MX Player
+27. ManoramaMAX
+28. Netflix
+29. Oho Gujarati
+30. Planet Marathi
+31. Pocket FM
+32. Prime Video
+33. PrimeShots
+34. ReelDrama
+35. STAGE
+36. Saina Play
+37. ShemarooMe
+38. SonyLiv
+39. Sun NXT
+40. Tarang Plus
+41. ULLU
+42. Ultra Jhakaas
+43. Vivamax
+44. Zee5
+
+More OTTs Coming Soon... 😘❤️"
+
+usage_TEXT = "Hi I am one and only DRM Downloader Bot on Telegram.
+
+💥 Send Any DRM Link I will upload it To Telegram or Google Drive.
+
+💥 I support Direct DRM link from Zee5, Hotstar, SonyLIV, JioCinema, Sunnxt, Atrangii, Etc....
+
+💥 You can also download from custom mpd link if you have the keys."
+
+plans_TEXT = "📞 Contact For Plan Details: @x1xEdiz"
