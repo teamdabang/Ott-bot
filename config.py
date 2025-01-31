b4cbb5e235f94995fd13e033d0d0e2c30056179b
teamdabang <dgjg45768@gmail.com>
2025-01-31 07:27:24 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import environ
+
+API_ID = int(environ.get("API_ID", "26910777"))
+API_HASH = environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7209702780:AAEC3bfXbhjvJdwm0xEllMmoeKMR0poQmj8")
+ADMIN = int(environ.get("ADMIN", "5232142502")         
